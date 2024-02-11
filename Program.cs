0f1c846c8c6948f9377e8cc43207407d1c6b5dfa
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-02-11 17:03:37 +0500

    Upload project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter first input: "); 
+string firstInput = Console.ReadLine();
+Console.Write("Enter second input: ");
+string secondInput = Console.ReadLine();
+Console.WriteLine($"{firstInput}dasdfas{secondInput}");
+
+Console.WriteLine("Converting your inputs...");
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+Console.WriteLine("Calculating operation...");
+Console.Write("RESULT: ");
+Console.WriteLine(firstNumber + secondNumber);
