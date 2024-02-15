4409c98d4babfe2adb1d54a0066040f55ea11c85
Ahmadxon Axmedov <153198605+Ahmadxon1990@users.noreply.github.com>
2024-02-16 01:15:45 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿Console.WriteLine("Fibanaci raqamni aniqlaydigan loyixa: "); // 10 ning fibonacilari => 1, 2, 3, 5, 8
-Console.Write("Enter a number: ");
-            string userStrInput = Console.ReadLine();
-            int userInput = Convert.ToInt32(userStrInput);
-            int num1 = 0; int num2 = 1; int result = 0;
-            for(int i = 0; i < userInput; i++)
-            {
-                result = num1 + num2; // result = 1;2;3;5;8;
-                num1 = num2; //num1 = 1;2;3;5
-                num2 = result;//1;2;3;5;8;
-                if(result <= userInput)
-                {
-                    Console.WriteLine(result);
-                }
-            }
+﻿Console.WriteLine("Kichik calculator loyixasi:   ");
+
+Console.Write("Birinchi raqamni kiriting:   ");
+decimal firstNumber = decimal.Parse(Console.ReadLine());
+
+Console.Write("Matematik belgini kiriting: ( + - * / % ):  ");
+char matemBelgi = char.Parse(Console.ReadLine());
+
+Console.Write("Ikkinchi raqamni kiriting   ");
+decimal secondNumber = decimal.Parse(Console.ReadLine());
+ 
+decimal result = matemBelgi switch 
+{
+   '+' => firstNumber + secondNumber,
+   '-' => firstNumber - secondNumber,
+   '*' => firstNumber * secondNumber,
+   '/' => firstNumber / secondNumber,
+   '%' => firstNumber % secondNumber,
+   _ => throw new ("Siz noto`g`ri matematik belgi kiritdingiz, qayta urinib ko`ring")
+   
+};
+
+Console.WriteLine($"RUSELT {firstNumber} {matemBelgi} {secondNumber} = {result} ");
